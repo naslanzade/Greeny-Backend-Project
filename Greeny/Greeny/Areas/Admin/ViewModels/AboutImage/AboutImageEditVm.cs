418729292e79bb7ec88f6094334f1968098d5202
naslanzade <novrastafa@code.edu.az>
2023-06-30 11:57:24 +0400

    add backup

---
+++ Greeny/Greeny/Areas/Admin/ViewModels/AboutImage/AboutImageEditVm.cs
@@ -0,0 +1,8 @@
+﻿namespace Greeny.Areas.Admin.ViewModels.AboutImage
+{
+    public class AboutImageEditVm
+    {
+        public string? Image { get; set; }
+        public IFormFile? NewImage { get; set; }
+    }
+}
